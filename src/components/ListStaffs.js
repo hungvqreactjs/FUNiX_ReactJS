ab9589ebe923ad699d8977b2227ceb8b65167dbe
Hung Vu <hungvq.rjs101x@gmail.com>
2021-10-21 02:55:50 +0700

    update tình huống 2 + Xóa file thừa

--- src/components/ListStaffs.js
+++ src/components/ListStaffs.js
@@ -1,35 +1,53 @@
-import React, {Component} from "react";
-import {Media} from 'reactstrap'
-
+import React, { Component } from "react";
+import { Card, CardBody, CardTitle, CardText } from "reactstrap";
 
 class List extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-        }
-    }
-    render(){
+  constructor(props) {
+    super(props);
+    this.state = {
+      InfoStaffs: null,
+    };
+  }
+  clickInfoStaffs(staffs) {
+    this.setState({ InfoStaffs: staffs });
+  }
 
-        const nhanVien = this.props.STAFFS.map((staffs) =>{
-            return(
-                <div key={staffs.id} className="col-6 mt-5">
-                    <Media tag="li">
-                        <h3>{staffs.name}</h3>
-                    </Media>
-                </div>
-            )
-        })
-        return(
-            <div className="container">
-                <div className="row">
-                    <Media liststaffs>
-                            {nhanVien}
-                    </Media>
-                </div>
-            </div>
-        );
-    }
+  renderinfo(staffs) {
+    if (staffs != null)
+      return (
+        <Card>
+          <CardBody>
+            <CardTitle>{staffs.name}</CardTitle>
+            <CardText>{staffs.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+  }
+
+  render() {
+    const nhanVien = this.props.STAFFS.map((staffs) => {
+      return (
+        <div key={staffs.id}>
+          <Card
+            className="col-sm-12 col-md-12"
+            onClick={() => this.clickInfoStaffs(staffs)}
+          >
+            <div>{staffs.name}</div>
+          </Card>
+        </div>
+      );
+    });
+    return (
+      <div className="container">
+        <div className="row">{nhanVien}</div>
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            {this.renderinfo(this.state.InfoStaffs)}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default List;
