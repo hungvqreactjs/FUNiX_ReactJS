75685e0c2b94c5fa6ddecbc70f9de97dcda77558
Hung Vu <hungvq.rjs101x@gmail.com>
2021-10-21 00:56:03 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
 import List from "./components/ListStaffs";
+import { STAFFS } from "./shared/staffs"
 
-function App() {
-  return (
-    <div className="App">
-      <List/>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state ={
+      STAFFS : STAFFS
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+         <List STAFFS={this.state.STAFFS} />
+      </div>
+    );
+  }
 }
+  
+
 
 export default App;
