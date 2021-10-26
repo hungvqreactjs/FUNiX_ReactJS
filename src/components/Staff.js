9df385061811dd78c411fec6f317840092d58642
Hung Vu <hungvq.rjs101x@gmail.com>
2021-10-26 21:04:39 +0700

    Creat Departments component

--- src/components/Staff.js
+++ src/components/Staff.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import { Card, CardImg, CardTitle } from "reactstrap";
 
 const Staff = ({staff,  onClick}) => {
     return (
