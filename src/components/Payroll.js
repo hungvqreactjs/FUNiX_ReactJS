a9a7b09398ec309ca091ebda76ff48a0a4af3741
Hung Vu <hungvq.rjs101x@gmail.com>
2021-10-27 03:36:24 +0700

    update Breadcrumb

--- src/components/Payroll.js
+++ src/components/Payroll.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { Card, CardTitle, CardText } from "reactstrap";
+import {
+  Card,
+  CardTitle,
+  CardText,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
-const Payroll =({props}) => {
-  const payroll =  props.map((payroll) => {
+const Payroll = ({ props }) => {
+  const payroll = props.map((payroll) => {
     return (
       <div className="col-12 col-md-5 m-1" key={payroll.id}>
         <Card onClick={payroll.id}>
@@ -10,17 +17,27 @@ const Payroll =({props}) => {
           <CardText>Mã Nhân Viên: {payroll.id}</CardText>
           <CardText>Hệ Sô lương: {payroll.salaryScale}</CardText>
           <CardText>Số ngày làm thêm {payroll.overTime}</CardText>
-          <CardText>Lương {2000*payroll.salaryScale + payroll.overTime*100}</CardText>
+          <CardText>
+            Lương {2000 * payroll.salaryScale + payroll.overTime * 100}
+          </CardText>
         </Card>
       </div>
     );
   });
-  return(
-      <div>
-        {payroll}
+  return (
+    <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/home">Home</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>Bảng Lương</BreadcrumbItem>
+        </Breadcrumb>
+        <hr />
       </div>
-  )
-}
-
+      <div className="row">{payroll}</div>
+    </div>
+  );
+};
 
 export default Payroll;
