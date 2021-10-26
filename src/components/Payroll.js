46f8fed01a260aebfe841791df515fc4c3786d5c
Hung Vu <hungvq.rjs101x@gmail.com>
2021-10-27 01:49:32 +0700

    fix error

--- src/components/Payroll.js
+++ src/components/Payroll.js
@@ -10,7 +10,7 @@ const Payroll =({props}) => {
           <CardText>Mã Nhân Viên: {payroll.id}</CardText>
           <CardText>Hệ Sô lương: {payroll.salaryScale}</CardText>
           <CardText>Số ngày làm thêm {payroll.overTime}</CardText>
-          <CardText>Lương {2000*payroll.salaryScale + payroll.overTime*1y00}</CardText>
+          <CardText>Lương {2000*payroll.salaryScale + payroll.overTime*100}</CardText>
         </Card>
       </div>
     );
