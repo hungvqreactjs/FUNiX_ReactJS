6c7281e0f9ce71ae720a9854a3e9d270d25a05f3
Hung Vu <hungvq.rjs101x@gmail.com>
2021-11-04 00:08:49 +0700

    Form

--- src/components/AddStaff.jsx
+++ src/components/AddStaff.jsx
@@ -1,41 +1,151 @@
-
-import React, {useState} from "react";
-import { Modal,ModalHeader,ModalBody,ModalFooter, Button } from "reactstrap";
+import React, { useState } from "react";
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Label,
+  Col,
+  Input,
+  Button,
+} from "reactstrap";
 
 const AddStaff = () => {
-  const [modal, setModal] = useState(false)
-  const toggle = () => setModal(!modal)
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
 
   return (
     <div>
-  <Button
-    onClick={toggle}
-  >
-   <i class="fa fa-user-plus "></i>
-  </Button>
-  <Modal
-    isOpen ={modal} toggle={toggle}
-  >
-    <ModalHeader>
-      Thêm nhân viên
-    </ModalHeader>
-    <ModalBody>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </ModalBody>
-    <ModalFooter>
-      <Button
-        color="primary"
-        onClick={function noRefCheck(){}}
-      >
-        Thêm
-      </Button>
-      {' '}
       <Button onClick={toggle}>
-        hủy
+        <i class="fa fa-user-plus "></i>
       </Button>
-    </ModalFooter>
-  </Modal>
-</div>
-  )}
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader>Thêm nhân viên</ModalHeader>
+        <ModalBody>
+          <div className="row modal-add">
+            <div className="col-12 col-md-9">
+              <Form >
+                <FormGroup row>
+                  <Label htmlFor="firstname" md={5}>
+                    Họ và tên
+                  </Label>
+                  <Col md={7}>
+                    <Input
+                      type="text"
+                      id="name"
+                      name="name"
+                      placeholder="Tên ..."
+                   
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label htmlFor="doB" md={5}>
+                    Ngày sinh
+                  </Label>
+                  <Col md={7}>
+                    <Input
+                      type="date"
+                      id="doB"
+                      name="doB"
+                      placeholder="ngày sinh"
+                    
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label htmlFor="startDate" md={5}>
+                    Ngày vào công ty
+                  </Label>
+                  <Col md={7}>
+                    <Input
+                      type="date"
+                      id="startDate"
+                      name="startDate"
+                      placeholder="Ngày vào công ty"
+                    
+                    />
+                  </Col>
+                </FormGroup>
 
-export default AddStaff
+                <FormGroup row>
+                  <Label htmlFor="department" md={6}>
+                  Phòng ban
+                  </Label>
+                  <Col md={6}>
+                  <Input
+                      type="select"
+                      name="department"
+                     
+                    >
+                      <option>Sale</option>
+                      <option>HR</option>
+                      <option>Marketing</option>
+                      <option>IT</option>
+                      <option>Finance</option>
+                      Finance
+                    </Input>
+                  </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                  <Label htmlFor="salaryScale" md={6}>
+                    Hệ số lương
+                  </Label>
+                  <Col md={6}>
+                    <Input
+                      type="text"
+                      id="salaryScale"
+                      name="salaryScale"
+                      placeholder="0"
+                    
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label htmlFor="overTime" md={6}>
+                    Sô ngày làm thêm
+                  </Label>
+                  <Col md={6}>
+                    <Input
+                      type="text"
+                      id="overTime"
+                      name="overTime"
+                      placeholder="0"
+                    
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label htmlFor="annualLeave" md={6}>
+                  Số ngày nghỉ còn lại
+                  </Label>
+                  <Col md={6}>
+                    <Input
+                      type="text"
+                      id="annualLeave"
+                      name="annualLeave"
+                      placeholder="0"
+                    
+                    />
+                  </Col>
+                </FormGroup>
+        
+              </Form>
+            </div>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={function noRefCheck() {}}>
+            Thêm
+          </Button>{" "}
+          <Button onClick={toggle}>hủy</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
+
+export default AddStaff;
--- src/pages/StaffDetail.jsx
+++ src/pages/StaffDetail.jsx
@@ -44,12 +44,12 @@ const StaffDetail = () => {
           </div>
           <CardBody className="col-lg-9 col-sm-8 col-12">
             <CardTitle className="card-title-info">{staff.name}</CardTitle>
+            <CardText>
+              Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}
+            </CardText>
             <CardText>Phòng ban: {staff.department.name}</CardText>
             <CardText>
               Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
-            </CardText>
-            <CardText>
-              Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}
             </CardText>
             <CardText>Số ngày làm thêm: {staff.overTime}</CardText>
             <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
