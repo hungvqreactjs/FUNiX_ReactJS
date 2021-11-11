b5ed573c7aa5407bef0b8b463c2f19cc67ad0875
Hung Vu <hungvq.rjs101x@gmail.com>
2021-11-12 00:06:32 +0700

    update

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,15 @@
-import {createStore} from 'redux';
-import { Reducer, initialState } from './reducer'
+import {createStore, combineReducers} from 'redux';
+import { Staff } from './staff'
+import { Department } from './department';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState, 
+        combineReducers ({
+            staff : Staff,
+            department : Department
+        })
+        
+       
     );
 
     return store;
---
+++ src/redux/staff.js
@@ -0,0 +1,8 @@
+import { STAFFS } from "../shared/staffs";
+
+export const Staff = (state = STAFFS, action) => {
+    switch (action.type) {
+        default:
+          return state;
+      }
+};
